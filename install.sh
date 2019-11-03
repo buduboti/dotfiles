b3cb33e65860f1d2edef004d1138d0e2ece7944c
Balás Botond Barna <buduboti@192-168-0-184.rdsnet.ro>
2019-11-03 15:57:41 +0200

    theme

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Clone the theme repo
+git clone https://github.com/chriskempson/base16-shell.git ./themes/base16-shell/
+
+# Apply the theme (there is a lot)
+chmod 755 ./themes/base16-shell/scripts/base16-gruvbox-dark-pale.sh && ./themes/base16-shell/scripts/base16-gruvbox-dark-pale.sh
+
+
