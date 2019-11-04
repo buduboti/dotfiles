ee9c837bdd9b1d473af871045f90919698edf470
Balás Botond Barna <buduboti@yahoo.com>
2019-11-04 16:02:02 +0200

    some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,13 @@
 
 alias c=clear
 alias q=exit
+alias :q=exit
 
 alias ls="ls -G"
 alias l="ls -lG"
 alias ll="ls -laG"
 
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
