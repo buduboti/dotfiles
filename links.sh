f749a6c6df25bd28df29700792a1c91f39161905
Balás Botond Barna <buduboti@yahoo.com>
2019-11-11 19:44:15 +0200

    links.sh uncommented

--- links.sh
+++ links.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ln -s .dotfiles/zsh/zshrc .zshrc
-ln -s .dotfiles/vim/vimrc .vimrc
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
