693a2443068bfdf90de7d2668c78a85abc80aafb
Balás Botond Barna <buduboti@yahoo.com>
2019-12-16 10:43:03 +0200

    battery with path

--- tmux/statusbar.sh
+++ tmux/statusbar.sh
@@ -51,7 +51,7 @@ set -g status-left "$tm_session_name"
 
 tm_tunes="#[bg=$base00,fg=$base0D] ♫ #(osascript -l JavaScript $DOTFILES/js/tunes.js)"
 # tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
-tm_battery="#[fg=$base0F,bg=$base00] #(battery)"
+tm_battery="#[fg=$base0F,bg=$base00] #($DOTFILES/bin/battery)"
 tm_date="#[default,bg=$base00,fg=$base0C] %R"
 tm_host="#[fg=$base0E,bg=$base00] #h "
 set -g status-right "$tm_tunes $tm_battery $tm_date $tm_host"
