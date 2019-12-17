9e8a6557087e1da09cbc4fc9f65a66d39c0f0470
Balás Botond Barna <buduboti@yahoo.com>
2019-12-17 13:59:19 +0200

    changed status bar

--- tmux/statusbar.sh
+++ tmux/statusbar.sh
@@ -23,13 +23,14 @@ set -g status-right-length 150
 set -g status-interval 5
 
 # default statusbar colors
-set-option -g status-style fg=$base01,bg=$base00,default
+set-option -g status-style fg=$base01,bg=$base04,default
+set-option -g status-bg $base04
 
-set-window-option -g window-status-style fg=$base03,bg=$base00
+set-window-option -g window-status-style fg=$base03,bg=$base04
 set-window-option -g window-status-format " #I #W"
 
 # active window title colors
-set-window-option -g window-status-current-style fg=$base0C,bg=$base00
+set-window-option -g window-status-current-style fg=$base0C,bg='#373737'
 set-window-option -g window-status-current-format " #[bold]#W"
 
 # pane border colors
@@ -37,7 +38,7 @@ set-window-option -g pane-active-border-style fg=$base0C
 set-window-option -g pane-border-style fg=$base03
 
 # message text
-set-option -g message-style bg=$base00,fg=$base0C
+set-option -g message-style bg=$base04,fg=$base0C
 
 # pane number display
 set-option -g display-panes-active-colour $base0C
@@ -46,11 +47,11 @@ set-option -g display-panes-colour $base01
 # clock
 set-window-option -g clock-mode-colour $base0C
 
-tm_session_name="#[default,bg=$base00,fg=$base0E] #S "
+tm_session_name="#[default,bg=$base04,fg=$base0E] #S "
 set -g status-left "$tm_session_name"
 
-tm_tunes="#[bg=$base00,fg=$base0D] ♫ #($DOTFILES/bin/music.sh)"
-tm_battery="#[fg=$base0F,bg=$base00] #($DOTFILES/bin/battery)"
-tm_date="#[default,bg=$base00,fg=$base0C] %R"
-tm_host="#[fg=$base0E,bg=$base00] #h "
+tm_tunes="#[bg=$base04,fg=$base0D] ♫ #($DOTFILES/bin/music.sh)"
+tm_battery="#[fg=$base0F,bg=$base04] #($DOTFILES/bin/battery)"
+tm_date="#[default,bg=$base04,fg=$base0C] %R"
+tm_host="#[fg=$base0E,bg=$base04] #h "
 set -g status-right "$tm_tunes $tm_battery $tm_date $tm_host"
