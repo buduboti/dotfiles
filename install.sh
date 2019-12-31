5597e20c09976da59b843875a241ccd5f70413dc
Balás Botond Barna <buduboti@yahoo.com>
2019-12-31 06:30:36 +0200

    zplug on ubuntu debugging

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	export LANGUAGE=en_US.UTF-8
 	export LANG=en_US.UTF-8
 	export LC_ALL=en_US.UTF-8
-	locale-gen en_US.UTF-8
+	sudo locale-gen en_US.UTF-8
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	brew install tmux vim curl zsh jq fzy
