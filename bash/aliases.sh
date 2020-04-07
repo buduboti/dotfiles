871c26e0fa0b94e89c06a488279e66c0b60576ae
Balas B. Botond Barna <bbim1594@linux.scs.ubbcluj.ro>
2020-04-07 11:06:27 +0300

    working bash config on fedora server

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -5,9 +5,9 @@ alias :q="exit"
 
 alias lg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-alias ls="ls -G"
-alias l="ls -lG"
-alias ll="ls -laG"
+alias ls="ls --color"
+alias l="ls -l --color"
+alias ll="ls -la --color"
 
 alias ...="cd ../.."
 alias ....="cd ../../.."
