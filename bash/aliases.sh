02a9f785655f51149a0057e94e6f460c9f3be172
Balás Botond Barna <buduboti@yahoo.com>
2020-04-07 10:28:08 +0300

    bashrc and aliases.sh

---
+++ bash/aliases.sh
@@ -0,0 +1,26 @@
+
+alias c="clear"
+alias q="exit"
+alias :q="exit"
+
+alias lg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+alias ls="ls -G"
+alias l="ls -lG"
+alias ll="ls -laG"
+
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+
+alias dot="cd $DOTFILES"
+
+alias grep="grep --color=always"
+
+alias path="echo $PATH | tr ':' '\n'"
+
+alias weather="curl wttr.in"
+
+# change directory aliases
+# source "$ZSH/cda.zsh"
