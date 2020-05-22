782ed864437c605690e808dd63ce9aec06cc964d
Balás Botond Barna <buduboti@yahoo.com>
2020-05-22 20:55:49 +0300

    .. alias added

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,7 @@ alias ls="ls -G"
 alias l="ls -lG"
 alias ll="ls -laG"
 
+alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
@@ -21,6 +22,3 @@ alias grep="grep --color=always"
 alias path="echo $PATH | tr ':' '\n'"
 
 alias weather="curl wttr.in"
-
-# change directory aliases
-# source "$ZSH/cda.zsh"
