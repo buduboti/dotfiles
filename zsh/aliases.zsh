6d0a0df378c3de546c78269689666fb2733f548c
Balás Botond Barna <buduboti@yahoo.com>
2020-09-22 14:43:32 +0300

    ls color for different os

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,9 +5,17 @@ alias :q=exit
 
 alias lg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-alias ls="ls -G"
-alias l="ls -lG"
-alias ll="ls -laG"
+if ls --color -d . >/dev/null 2>&1; then
+	alias ls="ls --color"
+	alias l="ls -l --color"
+	alias ll="ls -la --color"
+elif ls -G -d . >/dev/null 2>&1; then
+	alias ls="ls -G"
+	alias l="ls -lG"
+	alias ll="ls -laG"
+else
+    SOLARIS_LS=1
+fi
 
 alias ..="cd .."
 alias ...="cd ../.."
