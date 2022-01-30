0c00fd30df81517ebb25e982d20724de19508187
Balás Botond Barna <buduboti@yahoo.com>
2022-01-31 00:07:31 +0200

    npm & python3 added to install list

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ if [ -f '/usr/bin/apt' ]; then
 	sudo apt install -y vim
 	sudo apt install -y curl
 	sudo apt install -y zsh
+	sudo apt install -y python3
+	sudo apt install -y npm
 
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 	~/.fzf/install
@@ -25,14 +27,14 @@ elif [ -f '/usr/bin/pacman' ]; then
 	sudo pacman -Sy vim
 	sudo pacman -Sy curl
 	sudo pacman -Sy zsh
+	sudo pacman -Sy python3
+	sudo pacman -Sy npm
 
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 	~/.fzf/install
 
 elif [ -f '/opt/homebrew/bin/brew' ]; then
-	brew install tmux vim curl zsh jq fzf
-	brew install --HEAD luajit
-	brew install --HEAD neovim
+	brew install tmux vim curl zsh jq fzf python3 npm
 	brew install --HEAD luajit
 	brew install --HEAD neovim
 else
