9cd388372c776257d8a18ea768f191f2a6e4d765
Balás Botond Barna <balas.botond@codespring.ro>
2022-10-31 18:02:50 +0200

    zsh keybindings

---
+++ zsh/keybindings.zsh
@@ -0,0 +1,23 @@
+
+# https://www.gnu.org/software/bash/manual/html_node/Commands-For-History.html
+bindkey -M viins '^P' up-line-or-history
+bindkey -M viins '^N' down-line-or-history
+bindkey -M viins '^R' reverse-search-history
+bindkey -M viins '^S' forward-search-history
+bindkey -M viins '^Y' yank-last-arg
+
+
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '^x^e' edit-command-line
+
+bindkey -M viins '^_' insert-last-word
+
+# https://www.gnu.org/software/bash/manual/html_node/Commands-For-Moving.html
+bindkey -M viins '^A' beginning-of-line
+bindkey -M viins '^E' end-of-line
+bindkey -M viins '^F' forward-char
+bindkey -M viins '^B' backward-char
+bindkey -M viins 'ƒ' forward-word
+bindkey -M viins '∫' backward-word
+
